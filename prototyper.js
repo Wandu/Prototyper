a6f8f9a6a694725b0aff590f61223c78e7cfc62e
Changwan Jun <me@wani.kr>
2015-06-14 10:57:50 +0900

    add prototyper main

---
+++ prototyper.js
@@ -0,0 +1,65 @@
+;(function(global, factory){
+    if (typeof exports === "object") {
+        module.exports = factory();
+    }
+    else if ( typeof define === 'function' && define.amd ) {
+        define(factory);
+    }
+    else {
+        global.Prototyper = factory();
+    }
+})(this, function() {
+
+    var extend = function(origin, target) {
+        for (key in target) {
+            if (target.hasOwnProperty(key)) {
+                origin[key] = target[key];
+            }
+        }
+        return origin;
+    };
+
+    var Prototyper = function() {
+        this.construct.apply(this, arguments);
+    };
+    Prototyper.prototype.construct = function() {};
+    Prototyper.prototype.getBindedMethod = function(name) {
+        var bindedMethod = this[name];
+        var self = this;
+        return function() {
+            return bindedMethod.apply(self, arguments);
+        };
+    };
+    Prototyper.extend = function(methods) {
+
+        methods = methods || {};
+
+        var SubPrototyper = function() {
+            this.construct.apply(this, arguments);
+        };
+
+        extend(SubPrototyper, this);
+
+        // instance methods inheritance
+        SubPrototyper.prototype = Object.create(this.prototype, {
+            constructor: {
+                value: SubPrototyper,
+                enumerable: false,
+                writable: true,
+                configurable: true
+            }
+        });
+
+        extend(SubPrototyper.prototype, methods);
+
+        SubPrototyper.prototype.construct = methods.construct || this.prototype.construct;
+        SubPrototyper.prototype.super = this.prototype;
+
+        return SubPrototyper;
+    };
+    Prototyper.isParentOf = function(child) {
+        return (child instanceof this);
+    };
+
+    return Prototyper;
+});
